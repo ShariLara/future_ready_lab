ab1090454bccfaaf49ed0634de9057d2138f97ff
ShariLara <larashari02@gmail.com>
2018-06-26 11:58:44 -0700

    Did all 3

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -1,33 +1,52 @@
 /*eslint-env browser*/
 function playGame(){ 
-alert("First enter a low number, then  high number. Then, guess a random number between them.");
+    alert("First enter a low number, then high number. Then, guess a random number between them."); 
 
-//get the low and high bounds
-//uses parseInt() to make sure we have numbers not strings 
-var from = parseInt(prompt("Enter the lower bound."));
+    //get the low and high bounds
+    //uses parseInt() to make sure we have numbers not strings 
+    var from = parseInt(prompt("Enter the lower bound."));
+    while( isNaN(from || from < 0 || from > 1000)){
+            from = parseInt(prompt("Enter a number between 0 and 1000"))
+    }
 
-var to = parseInt(prompt ("Enter the higher bound."));
+    var to = parseInt(prompt ("Enter the higher bound."));
+    while( isNaN(from || from < 0 || from > 1000)){
+            from = parseInt(prompt("Enter a number between 0 and 1000"))
+                 }
 
-//get an integer between [from, to]
-//Math.random() returns decimals, used Math.round() to get whole number 
-var target = Math.round(Math.random() * (to - from) + from);
 
-var currentGuess = parseInt(prompt("Guess a number between " + from + " and " + to));
+    //get an integer between [from, to]
+    //Math.random() returns decimals, used Math.round() to get whole number 
+    var target = Math.round(Math.random() * (to - from) + from);
 
-var totalGuesses = 1;
+    var currentGuess = parseInt(prompt("Guess a number between " + from + " and " + to));
 
-//loop until user guesses correct number 
-while(currentGuess != target){ 
-    if (currentGuess < target){
-    currentGuess = parseInt(prompt ("Enter a higher number"));
-        
-        totalGuesses ++;
-    }else if (currentGuess > target) {
-        currentGuess = parseInt(prompt("Enter a lower number"));
+    var totalGuesses = 1;
+
+
+    //loop until user guesses correct number 
+    while(currentGuess != target){ 
+        if (currentGuess < target){
+        currentGuess = parseInt(prompt ("Enter a higher number"));
+
+            totalGuesses ++;
+        }else if (currentGuess > target) {
+            currentGuess = parseInt(prompt("Enter a lower number"));
+
+            totalGuesses ++;
+        }  
+        while(currentGuess > to || currentGuess < from){
+            currentGuess = parseInt(prompt ("Enter number between" +from+ " and " +to));
+        }
+
+        while (isNaN(currentGuess)){
+            currentGuess = parseInt(prompt ("Enter a numberical value"))
+        }
+
+    }
     
-        totalGuesses ++;
-    }   
-}  
-
     alert ("It took " + totalGuesses + "tries to guess the correct number." );
 }
+
+
+//entered value is number
