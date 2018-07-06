846df40526a173a343bb4bcaaa424b62b76b09d1
ShariLara <larashari02@gmail.com>
2018-07-06 13:03:30 -0700

    I edited the sketch.js file.

--- Week_2/Processing JS/trying.js
+++ Week_2/Processing JS/trying.js
@@ -25,41 +25,29 @@ var ballList = [];
 
 function setup() {
     createCanvas(parseInt(prompt("Input a number for the width of the canvas.")), parseInt(prompt("Input a number for the height of the canvas.")));
-    var background = [];
     var numCircle = random([2,5,10,16]);
     for(var i = 0; i < numCircle; i++) {
-        ballList[i] = new Ball(random(0, width), random(0, height), 15);
+        ballList[i] = new Ball(random(0, width), random(0, height), 20);
     }
-} 
+}
 
 function draw() {
-    background(210, 135, 146); //sets background color to pink
-    for(var i = 0; i < ballList.length; i++) {
+    background(210, 135, 146);
+    for(var i = 0; i < ballList.length; i++){
         fill(ballList[i].color); 
         ellipse(ballList[i].xCoor, ballList[i].yCoor, ballList[i].diameter);
-
-        // if the xCoord of circle is greater than the length of the canvas, bounce back
-        if(ballList[i].xCoor > width){
-            ballList[i].diameter = random(20, 80);
+         //if the xCoord of circle is greater than the length of the canvas, bounce back
+        if(ballList[i].xCoor > width || ballList[i].xCoor < 0){
             ballList[i].color = [random(0,255), random(0,255), random(0,255)];
             ballList[i].xSpeed = -ballList[i].xSpeed;
-        } else if (ballList[i].xCoor < 0){
-            ballList[i].xSpeed = -ballList[i].xSpeed;
-           
-        }
 
-        //ballList[i].xCoor += ballList[i].xSpeed;
-
-        if(ballList[i].yCoor > height){
-            ballList[i].ySpeed = -ballList[i].ySpeed;
-            ballList[i].color = [random(0,255), random(0,255), random(0,255)];
-        } else if (ballList[i].yCoor < 0){
-            ballList[i].ySpeed = -ballList[i].ySpeed;
-            
-            
-        }
-            ballList[i].xCoor += ballList[i].xSpeed;
-            ballList[i].yCoor += ballList[i].ySpeed;
         
+
+        } else if(ballList[i].yCoor > height || ballList[i].yCoor < 0){
+            ballList[i].ySpeed = -ballList[i].ySpeed;
+            ballList[i].diameter = random(20, 80);
+        }
+        ballList[i].xCoor += ballList[i].xSpeed;
+        ballList[i].yCoor += ballList[i].ySpeed;
     }
 }
